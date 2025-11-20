1bc6aad4b648f2689ca64df301b71555a639d848
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2025-11-20 10:18:28 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ resource "random_pet" "name" {
  length    = "47"
  separator = "-"
 }
+
+#resource "null_resource" "ls" {
+  #provisioner "local-exec" {
+    #command = "ls -la"
+  #}
+  #triggers = {
+    #run_every_time = uuid()
+  #}
+#}
